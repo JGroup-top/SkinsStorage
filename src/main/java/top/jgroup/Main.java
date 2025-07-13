b6194e9af6108a35b6874613361b3492caca3cb0
The_JCN <caxashuklin@mail.ru>
2025-07-13 13:31:41 +0500

    Try to deploy x2 [deploy]

--- src/main/java/top/jgroup/Main.java
+++ src/main/java/top/jgroup/Main.java
@@ -44,5 +44,6 @@ public class Main {
         app.start(Integer.parseInt(URL.split(":")[2]));
 
         System.out.println("Server started at " + URL);
+        System.out.println("Ура авто деплой работает!");
     }
 }
